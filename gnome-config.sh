ba7655ac54f005729e92de942838c93000d3d9f1
Matthew Leeds <mwl458@gmail.com>
2016-10-26 13:06:17 -0500

    Add some gsettings customizations

--- gnome-config.sh
+++ gnome-config.sh
@@ -13,3 +13,18 @@ gsettings set org.gnome.desktop.interface clock-show-date true
 
 # enable the GTK+ Inspector keyboard shortcut
 gsettings set org.gtk.Settings.Debug enable-inspector-keybinding true
+
+# set a couple keyboard shortcuts (this assumes no custom ones have already been set)
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Launch Terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Launch Nautilus'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'nautilus'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Control><Alt>n'
+
+# disable the annoying sound effects
+gsettings set org.gnome.desktop.sound event-sounds false
+
+# add the Dvorak keyboard layout
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+dvorak'), ('xkb', 'us')]"
