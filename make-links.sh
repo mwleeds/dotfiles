be895dce3e65b65199f75c26054f462131ae4c6b
Matthew Leeds <mwl458@gmail.com>
2016-10-26 13:20:45 -0500

    Add a script to make symbolic links for these dotfiles

---
+++ make-links.sh
@@ -0,0 +1,9 @@
+ln -sf ~/Desktop/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/Desktop/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Desktop/dotfiles/.jhbuildrc ~/.jhbuildrc
+ln -sf ~/Desktop/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/Desktop/dotfiles/.Xmodmap ~/.Xmodmap
+rm -rf ~/.vim && mkdir ~/.vim
+ln -sf ~/Desktop/dotfiles/.vim/plugin ~/.vim
+ln -sf ~/Desktop/dotfiles/.vim/doc ~/.vim
+
